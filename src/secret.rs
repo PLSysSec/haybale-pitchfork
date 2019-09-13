7706c58cc710641579ed3f78a7130a266abdb54f
Craig Disselkoen <craigdissel@gmail.com>
2019-09-13 13:57:30 -0700

    fix shadowmem constancy for boolector
    All tests passing

--- src/secret.rs
+++ src/secret.rs
@@ -4,6 +4,7 @@
 //! `haybale::memory::Memory`, or `boolector::BV`.
 
 use boolector::{Btor, BVSolution};
+use haybale::PossibleSolutions;
 use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
@@ -387,20 +388,33 @@ impl haybale::backend::Memory for Memory {
         match index {
             BV::Public(index) => {
                 let shadow_cell = haybale::backend::Memory::read(&self.shadow_mem, index, bits);
-                match shadow_cell.as_u64() {
-                    Some(0) => {
-                        // All bits being read are public
-                        BV::Public(haybale::backend::Memory::read(&self.mem, index, bits))
+                // In Boolector, reads on a constant array that return the default value are
+                // nonetheless not constant (they are merely constrained to be equal to the
+                // default value). So, we actually need to do a solve here.
+                // TODO consider how to make this more efficient.
+                match haybale::get_possible_solutions_for_bv(self.btor.btor.clone(), &shadow_cell, 1).unwrap() {
+                    PossibleSolutions::PossibleSolutions(hs) => match hs.len() {
+                        0 => panic!("No possible solution for the shadow bits"),
+                        1 => {
+                            let shadowbits = hs.iter().next().unwrap();
+                            if shadowbits.as_01x_str().chars().all(|c| c == '0') {
+                                // All bits being read are public
+                                BV::Public(haybale::backend::Memory::read(&self.mem, index, bits))
+                            } else {
+                                // Some or all of the bits are secret, so we treat the resulting value as entirely secret
+                                BV::Secret { btor: self.btor.clone(), width: bits, symbol: None }
+                            }
+                        },
+                        n => panic!("Expected at most one solution since passed in n==1, but got {} solutions", n),
                     },
-                    Some(_) | None => {
-                        // Some(_): some or all of the bits are secret, so we treat the resulting value as entirely secret
-                        // None: the bits' secrecy is non-constant; i.e., the bits could be secret or not,
+                    PossibleSolutions::MoreThanNPossibleSolutions(_) => {
+                        // The bits' secrecy is non-constant; i.e., the bits could be secret or not,
                         // depending on the values of other variables. This can happen, e.g., when reading
                         // from a symbolic address that could point to either secret or public data.
                         // We are interested in the "worst case", so since the resulting value _could be_
                         // secret, we follow the case where it _is_ secret.
                         BV::Secret { btor: self.btor.clone(), width: bits, symbol: None }
-                    }
+                    },
                 }
             },
             BV::Secret { btor, .. } => {
